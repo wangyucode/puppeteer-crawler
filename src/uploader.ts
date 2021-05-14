16a1ef96b5808ead7f03b5d8a88655d19d64123c
王郁 <wangyu@wycode.cn>
2021-05-15 00:42:37 +0800

    match

--- src/uploader.ts
+++ src/uploader.ts
@@ -4,13 +4,31 @@ import { DotaNews, DotaNewsNode } from "./types";
 const server = process.env.ENV === 'prod' ? "https://wycode.cn" : "http://localhost:8082";
 let token = "";
 
+export async function uploadSchedules(schedules: any[]) {
+    const res: any = await axios.put(`${server}/node/admin/dota/schedules`, schedules, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    console.log("uploadSchedule->", res.data);
+}
+
+export async function uploadTeams(teams: any[]) {
+    const res: any = await axios.put(`${server}/node/admin/dota/teams`, teams, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    console.log("uploadTeams->", res.data);
+}
+
 export async function uploadLeagues(leagues: any[]) {
     const res: any = await axios.put(`${server}/node/admin/dota/leagues`, leagues, {
         headers: {
             Authorization: `Bearer ${token}`
         }
     });
-    console.log("uploadNews->", res.data);
+    console.log("uploadLeagues->", res.data);
 }
 
 export async function uploadNews(news: DotaNews[]) {
