43b1f70003d74f7e035cd52d1fad17ab2bafccff
yu.wang <wangyu@wycode.cn>
2022-04-15 17:50:59 +0800

    image proxy update

--- src/utils.ts
+++ src/utils.ts
@@ -5,10 +5,17 @@ export function sleep(ms: number): Promise<number> {
 }
 
 const staticUrl = Buffer.from('aHR0cHM6Ly9pbWcuZG90YTIuY29tLmNu', "base64").toString('utf-8');
+const staticUrl1 = Buffer.from('aHR0cHM6Ly9zdGF0aWMucHdlc3BvcnRzLmNu', "base64").toString('utf-8');
 // console.log(staticUrl);
 
 export function convertImageUrl(url: string): string {
-    return url.startsWith(staticUrl) ? 'https://wycode.cn' + url.substring(24) : url;
+    if(url.startsWith(staticUrl)){
+        return 'https://wycode.cn' + url.substring(24);
+    } else if(url.startsWith(staticUrl1)){
+        return 'https://wycode.cn' + url.substring(24);
+    } else {
+        return url;
+    }
 }
 
 export function getBehaviorName(e) {
--- test.ts
+++ test.ts
@@ -1,1 +1,89 @@
-console.log(Buffer.from('https://m.weibo.cn/api/container/getIndex', 'utf-8').toString('base64'))
+import { convertImageUrl, getTalentString } from "./src/utils"
+
+// console.log(Buffer.from('https://static.pwesports.cn', 'utf-8').toString('base64'))
+
+console.log(convertImageUrl('https://img.dota2.com.cn/dota2static/dota2/6251c8c8-20fa-490a-a706-7c7b0da6662e.jpg'));
+
+const t1 = {
+    "id": 6299,
+    "name": "special_bonus_magic_resistance_12",
+    "name_loc": "+{s:value}% 魔法抗性",
+    "desc_loc": "",
+    "lore_loc": "",
+    "notes_loc": [],
+    "shard_loc": "",
+    "scepter_loc": "",
+    "type": 2,
+    "behavior": "2",
+    "target_team": 0,
+    "target_type": 0,
+    "flags": 0,
+    "damage": 0,
+    "immunity": 0,
+    "dispellable": 0,
+    "max_level": 4,
+    "cast_ranges": [
+        0
+    ],
+    "cast_points": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "channel_times": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "cooldowns": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "durations": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "damages": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "mana_costs": [
+        0,
+        0,
+        0,
+        0
+    ],
+    "gold_costs": [],
+    "special_values": [
+        {
+            "name": "value",
+            "values_float": [
+                12
+            ],
+            "is_percentage": false,
+            "heading_loc": "",
+            "bonuses": []
+        }
+    ],
+    "is_item": false,
+    "ability_has_scepter": false,
+    "ability_has_shard": false,
+    "ability_is_granted_by_scepter": false,
+    "ability_is_granted_by_shard": false,
+    "item_cost": 0,
+    "item_initial_charges": 0,
+    "item_neutral_tier": 4294967295,
+    "item_stock_max": 0,
+    "item_stock_time": 0,
+    "item_quality": 0
+}
+
+console.log(getTalentString(t1));
